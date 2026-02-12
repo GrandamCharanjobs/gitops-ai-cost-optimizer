1f3300b2b62afa22fedc74b84797d49b72d132c1
Charan Kumar Grandam <charankumargrandam@Mac.ht.home>
2026-02-11 18:20:04 -0600

    Live EKS demo: /mo t3.micro waste + 100+ OutOfpods demo

---
+++ terraform/main.tf
@@ -0,0 +1,41 @@
+provider "aws" {
+  region = "us-west-2"
+}
+
+module "vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "~> 5.0"
+
+  name            = "cost-optimizer-vpc"
+  cidr            = "10.0.0.0/16"
+  azs             = ["us-west-2a", "us-west-2b"]
+  private_subnets = ["10.0.1.0/24", "10.0.2.0/24"]
+  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24"]
+
+  enable_nat_gateway   = true
+  single_nat_gateway   = true
+  enable_vpn_gateway   = false
+}
+
+module "eks" {
+  source  = "terraform-aws-modules/eks/aws"
+  version = "~> 20.0"
+
+  cluster_name    = "cost-optimizer"
+  cluster_version = "1.30"
+
+  vpc_id     = module.vpc.vpc_id
+  subnet_ids = module.vpc.private_subnets
+
+  # bypass CloudWatch log-group conflicts
+  create_cloudwatch_log_group = false
+
+  eks_managed_node_groups = {
+    workers = {
+      min_size     = 1
+      max_size     = 1
+      desired_size = 1
+      instance_types = ["t3.micro"]
+    }
+  }
+}
